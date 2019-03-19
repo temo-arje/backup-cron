a15d7f35197b629d70033efc5389a9dba1186c37
Temo Arjevanidze <35239361+temo-arje@users.noreply.github.com>
2019-03-19 16:26:32 +0400

    Update CronBackup.sh

--- CronBackup.sh
+++ CronBackup.sh
@@ -7,8 +7,7 @@ sudo zip -r Backup.Arjesoft.$NOW.zip /var/www
 echo "Backup Added"
 ## Export Mysql All ##
 TODAY=`date +"%d%b%Y"`
-MYSQL_USER='root'
-MYSQL_PASSWORD='Arjestudio01'
-DATABASE_NAME='admin'
+MYSQL_USER='Mysql User'
+MYSQL_PASSWORD='Mysql Password'
 mysqldump --all-databases -u ${MYSQL_USER} -p ${MYSQL_PASSWORD} | gzip> SQL-${TODAY}-AllWebsites.sql.gz
 echo "All Database Exported"
